c126ec13ee48a4d996f0f4071ff186f1efd3589a
Jack You <gmyou71@gmail.com>
2013-07-11 22:49:48 +0900

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,24 +1,33 @@
-lat = [18.0, 64.0] 
-lng = [-124.0, -66.0]
-
-loc = []
-locs = []
-
-i=0.0
-
-arrLat = []
-for x in range(int((lat[1]-lat[0])*10)):
-    arrLat.append(lat[0]+x*0.1)  
+from itertools import product
+from sys import float_info
+ 
+_float_min = float_info.min
+_epsilon = float_info.epsilon
+ 
+def float_eq(a, b):
+    """ floating point comparison from http://floating-point-gui.de/"""
+    absA = abs(a)
+    absB = abs(b)
+    diff = abs(a - b)
     
-arrLng = []
-for x in range(int((lng[1]-lng[0])*10)):
-    arrLng.append(lng[0]+x*0.1)    
-
-
-locs = [(x, y) for x in arrLat for y in arrLng if x != y]  
-
-i = 0
-
-for loc in reversed(locs):
-    i += 1
+    if a == b:
+        return True
+    elif a == 0 or b == 0 or diff < _float_min:
+        return diff < (_epsilon * _float_min)
+    else:
+        return diff / (absA + absB) < _epsilon
+ 
+def xxrange(start, stop, step=1):
+    cur = start
+    while cur < stop:
+        yield cur
+        cur += step
+ 
+step = 0.1
+lats = xxrange(18.0, 64.0, step)
+lngs = xxrange(-124.0, -66.0, step)
+ 
+locs = reversed([(lat, lng) for lat, lng in product(lats, lngs) if not float_eq(lat, lng)])
+ 
+for i, loc in enumerate(locs):                                                  
     print i, loc
