7f708f047d338896c347baf524dd0513c29b33f5
ec2-user <gmyou71@gmail.com>
2013-07-12 07:33:26 +0000

    add thread

---
+++ thread.py
@@ -0,0 +1,38 @@
+from itertools import product
+from sys import float_info
+from multiprocessing import Process
+
+
+_float_min = float_info.min
+_epsilon = float_info.epsilon
+ 
+def float_eq(a, b):
+    absA = abs(a)
+    absB = abs(b)
+    diff = abs(a - b)
+    
+    if a == 0 or b == 0 or diff < _float_min:
+        return diff < (_epsilon * _float_min)
+    else:
+        return diff / (absA + absB) < _epsilon
+ 
+def xxrange(start, stop, step=1):
+    cur = start
+    while cur < stop:
+        yield cur
+        cur += step
+
+def f(names):
+    print names
+
+if __name__ == '__main__':
+     
+    step = 0.1
+    lats = xxrange(18.0, 64.0, step)
+    lngs = xxrange(-124.0, -66.0, step)
+     
+    locs = reversed([(round(lat, 1), round(lng, 1)) for lat, lng in product(lats, lngs) if not float_eq(lat, lng)])
+
+    p = Process(target=f, args=locs)
+    p.start()
+    p.join()
